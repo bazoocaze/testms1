02587a49b19590610d3cd30de555d9d6c6b8952d
Jasf <bazoocaze@gmail.com>
2026-07-22 20:32:23 -0300

    feat: consume testlib2 getFullAnswer() method

--- src/main/java/com/github/bazoocaze/ms/Main.java
+++ src/main/java/com/github/bazoocaze/ms/Main.java
@@ -1,13 +1,16 @@
 package com.github.bazoocaze.ms;
 
 import com.github.bazoocaze.lib.App;
+import com.github.bazoocaze.lib2.App2;
 
 /**
- * Simple microservice that calls testlib1 and prints a hello world.
+ * Simple microservice that calls testlib1 and testlib2 and prints a hello world.
  */
 public class Main {
     public static void main(String[] args) {
-        int answer = App.getAnswer();
+        String answer = App.getAnswer();
+        String fullAnswer = App2.getFullAnswer();
         System.out.println("Hello World! The answer is " + answer);
+        System.out.println("testlib2 says: \"" + fullAnswer + "\"");
     }
 }
